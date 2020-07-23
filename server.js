b5392def3e66db0b00580fea7017bff3872ebc95
Franco Carini <carini.franco@gmail.com>
2020-07-23 18:07:16 -0300

    Added db connect file, cookieParser, express session config

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express')
 const dotenv = require('dotenv')
 //Load env vars
 dotenv.config({path: './config/config.env'})
+const mongoose = require('mongoose')
+const connectDB = require('./config/db')
+connectDB()
+
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 const exphbs = require('express-handlebars')
 const path = require('path')
 
@@ -20,6 +27,17 @@ app.set('view engine', 'handlebars')
 
 // Static Files
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(cookieParser())
+
+// Session config
+app.use(session({
+  secret: process.env.SECRET,
+  key: process.env.KEY,
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
+}))
 
 // Mount Routes
 app.use(indexRoutes)
