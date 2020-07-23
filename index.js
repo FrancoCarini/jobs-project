3b2f6c1475babacf30887cb08d7ffe7ada4fde60
Franco Carini <carini.franco@gmail.com>
2020-07-23 14:34:32 -0300

    Added handlebars template engine, define static routes and add public folder

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express = require('express')
 const dotenv = require('dotenv')
 //Load env vars
 dotenv.config({path: './config/config.env'})
+const exphbs = require('express-handlebars')
+const path = require('path')
 
 const app = express()
+
+// Use Engine View Handlebars
+app.engine('handlebars', 
+  exphbs({
+    defaultLayout: 'layout'
+  })
+)
+app.set('view engine', 'handlebars')
+
+// Static Files
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(process.env.PORT, () => {
   console.log('Server running')
